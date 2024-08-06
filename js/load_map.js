7aba9b997a95caaa9fb1d1597ee6459adc4032ab
vschugr <vschugr@gmail.com>
2024-08-06 17:43:29 +0200

    123

--- js/load_map.js
+++ js/load_map.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Загрузка SVG файла
-    fetch('./svg/bundeslands.svg')
+function loadAndProcessSVG(containerId, svgPath) {
+    const svgContainer = document.getElementById(containerId);
+
+    fetch(svgPath)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -9,23 +10,58 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             // Вставка SVG содержимого в контейнер
-            document.getElementById('svg-container').innerHTML = data;
+            svgContainer.innerHTML = data;
+            // Получение SVG элемента
+            const svg = svgContainer.querySelector('svg');
+            if (svg) {
+                svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+                svg.style.maxWidth = '30%';
+                svg.style.maxHeight = '30%';
+                svg.style.width = '100%';
+                svg.style.height = 'auto';
+               
+            }
 
-            // Выполнение скрипта для обработки элементов path
-            const paths = document.querySelectorAll("svg path");
+            // Выполнение скрипта для обработки элементов path и polygon
+            const elements = svgContainer.querySelectorAll("svg path, svg polygon");
 
-            paths.forEach(path => {
-                const bbox = path.getBBox();
+            elements.forEach(element => {
+                const bbox = element.getBBox();
                 const cx = bbox.x + bbox.width / 2;
                 const cy = bbox.y + bbox.height / 2;
 
-                path.style.transformOrigin = `${cx}px ${cy}px`;
+                element.style.transformOrigin = `${cx}px ${cy}px`;
 
-                path.addEventListener("mouseover", function () {
+                element.addEventListener("mouseover", function () {
                     // Перемещаем элемент в конец родительского элемента
-                    path.parentNode.appendChild(path);
+                    element.parentNode.appendChild(element);
                 });
             });
         })
         .catch(error => console.error('Ошибка загрузки SVG:', error));
+}
+
+function addSwiper(){
+
+    const swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Инициализация Swiper
+   
+    addSwiper()
+    // Загрузка и обработка SVG файлов
+    loadAndProcessSVG('svg-container_1', 'images/federalstates.svg');
+    loadAndProcessSVG('svg-container_2', 'images/landdistings.svg');
 });
